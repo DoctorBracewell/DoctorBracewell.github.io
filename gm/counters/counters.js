f572eb5d479834b9dc88fea64e6b699c1654617d
DrBracewell <dragonclawslice1@gmail.com>
2020-06-05 19:20:25 +0100

    dsad

--- gm/counters/counters.js
+++ gm/counters/counters.js
@@ -57,6 +57,8 @@ function generateStuff() {
     if (parseInt(itemNum) > 1) {
         cmds.push("\n--- CMDS When Activated ---\n")
         cmds.push(`/fill ${parseInt(chest[0])+1} ${parseInt(chest[1])+1} ${parseInt(chest[2])} ${parseInt(chest[0])+6} ${parseInt(chest[1])+1} ${parseInt(chest[2])+1} diamond_block 0 replace gold_block 0`)
+    } else {
+        cmds.push("// \\/ Add small delay (1 repeater tick) before this command \\/")
     }
 
     cmds.push(`/entitydata @e[type=ArmorStand,r=1,x=${x},y=${y},z=${z}] {CustomNameVisible:0b}`);
@@ -70,7 +72,6 @@ function generateStuff() {
     cmds.push(`/setblock x y z gold_block`)
     cmds.push(`/entitydata @e[type=ArmorStand,r=1,x=${x},y=${y},z=${z}] {CustomNameVisible:1b}`);
     cmds.push(`/clone ${chest[0]} ${parseInt(chest[1]) + 1} ${chest[2]} ${chest[0]} ${parseInt(chest[1]) + 1} ${chest[2]} ${chest[0]} ${chest[1]} ${chest[2]}`);
-    cmds.push("// \\/ Add small delay (1 repeater tick) before this command \\/")
     cmds.push(`/entitydata @e[type=ArmorStand,r=1,x=${x},y=${y},z=${z},tag=thTestCounter] {CustomName:"&a0&2/${itemNum}"}`);
 
     commandsOut.getDoc().setValue(cmds.join("\n\n"));
