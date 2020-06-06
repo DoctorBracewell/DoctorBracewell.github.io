caff9ec012778193bb1956093637da8499dd552d
DrBracewell <dragonclawslice1@gmail.com>
2020-06-06 16:10:24 +0100

    reorganisation!

--- gm/counters/counters.js
+++ gm/counters/counters.js
@@ -1,12 +1,3 @@
-// Cookies
-if (localStorage.getItem('cookies') === null) {
-    document.querySelector("#cookies-popup").style.display = "flex";
-}
-document.querySelector(".cookies-accept").addEventListener("click", () => {
-    localStorage.setItem("cookies", true);
-    document.querySelector("#cookies-popup").style.display = "none";
-})
-
 // Storage
 if (typeof(Storage) !== "undefined") {
     
