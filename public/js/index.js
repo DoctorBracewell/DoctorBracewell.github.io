b421560c3f04675704e202e93961f432fbc93dcd
DrBracewell <dragonclawslice1@gmail.com>
2020-11-28 17:41:37 +0000

    Huge Revamp

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+
+function change() {
+    var bgcolour = ('#' + (Math.random() * 0xFFFFFF << 0).toString(16));
+    document.body.style.backgroundColor = bgcolour;
+}
+
+change()
+
+setInterval(change, 2000);
