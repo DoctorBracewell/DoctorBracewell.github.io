734d92c76e02bfefce9dbfa30b63fa02d7ab4c92
DrBracewell <doctorbracewell@gmail.com>
2022-03-02 18:29:08 +0000

    Update imageTilts.ts

--- src/imageTilts.ts
+++ src/imageTilts.ts
@@ -1,9 +1,12 @@
 import VanillaTilt from "vanilla-tilt";
 
-VanillaTilt.init([...document.querySelectorAll(".image-tilt"]), {
-  reverse: true,
-  max: 7,
-  speed: 400,
-  scale: 1.03,
-  glare: true,
-});
+VanillaTilt.init(
+  [...(document.querySelectorAll(".image-tilt") as NodeListOf<HTMLElement>)],
+  {
+    reverse: true,
+    max: 7,
+    speed: 400,
+    scale: 1.03,
+    glare: true,
+  }
+);
